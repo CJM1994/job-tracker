66f455e57cc9444ae7630dc5ecbd54e309b032cc
CJM1994 <connorjohnmullin@gmail.com>
2022-03-26 22:39:08 -0700

    Start framework for file uploader component

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 export default function Uploader() {
 
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const fileSelected = event => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   return (
-    <button>Uploader</button>
+    <>
+      <input type='file' onChange={fileSelected}></input>
+      <button>Upload File</button>
+    </>
   )
 
 };
